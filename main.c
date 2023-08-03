bfabf6f0eddb3d5ab4e6b647fd0225e040a0be2f
alex_s168 <63254202+SuperCraftAlex@users.noreply.github.com>
2023-08-03 12:50:43 +0200

    prep

--- main.c
+++ main.c
@@ -3,15 +3,21 @@
 #include <string.h>
 #include <unistd.h>
 
+typef unsigned char byte;
+
 #define mode32 (mode == 32)
 #define mode64 (mode == 64)
+
+#define write_asm(x) if(gen_sources) {fputs(x, fp);}
+#define writef_asm(...) if(gen_sources) {fprintf(fp, __VA_ARGS__);}
+
+#define write_byte(x) if(!gen_sources) {fwrite(&x, sizeof(byte), 1, fp);}
 
 #define warn(x) printf(x); \
     if (werror) {          \
         return 1;          \
     }
 
-// generates nasm code
 int main(int argc, char **argv) {
     int werror = 0;
 
@@ -46,6 +52,7 @@ int main(int argc, char **argv) {
         }
     }
 
+    // todo: automatize
     if (cell_off < 0) {
         cell_off = memsize / 2;
     }
@@ -65,7 +72,7 @@ int main(int argc, char **argv) {
     fread(buff, size, size, fp);
     fclose(fp);
 
-    // strip symbols
+    // strip comments
     char *buff2 = malloc(size);
     int y = 0;
     for (size_t i = 0; i < size; i++) {
@@ -80,21 +87,13 @@ int main(int argc, char **argv) {
     buff[y] = 0;
     size = y;
 
-    char fn[100];
-    if (!gen_sources) {
-        strcpy(fn, argv[argc-2]);
-        strcat(fn, ".nasm");
-    }
-    else {
-        strcpy(fn, argv[argc-1]);
-    }
+    char *fn = argv[argc-1];
     fp = fopen(fn, "w");
     int localid = 0;
 
-    // nasm header
-    fputs("section .data\ncells:\n", fp);
-    fprintf(fp, "    times %i db 0\n\n", memsize);
-    fprintf(fp, "section .text\n    global _start\n\n_start:\n    mov ecx, cells+%i\n\n", cell_off);
+    write_asm("section .data\ncells:\n");
+    writef_asm("    times %i db 0\n\n", memsize);
+    writef_asm("section .text\n    global _start\n\n_start:\n    mov ecx, cells+%i\n\n", cell_off);
     for (size_t i = 0; i < size; i++) {
         char c = buff[i];
         if (c == '\n') {
@@ -102,38 +101,38 @@ int main(int argc, char **argv) {
         }
         if (mode32) {
             if (c == '>') {         // increment pt
-                fputs("    inc ecx\n", fp);
+                write_asm("    inc ecx\n");
             }
             else if (c == '<') {    // decrement pt
-                fputs("    dec ecx\n", fp);
+                write_asm("    dec ecx\n");
             }
             else if (c == '+') {    // inc cell at pt
                 int am = (int)strspn(buff + i, "+");
                 if (am > 1) {
-                    fprintf(fp, "    add byte [ecx], %i\n", am);
+                    writef_asm("    add byte [ecx], %i\n", am);
                     i += am-1;
                     continue;
                 } else {
-                    fputs("    inc byte [ecx]\n", fp);
+                    write_asm("    inc byte [ecx]\n");
                 }
             }
             else if (c == '-') {    // dec cell at pt
                 int am = (int)strspn(buff + i, "-");
                 if (am > 1) {
-                    fprintf(fp, "    sub byte [ecx], %i\n", am);
+                    writef_asm("    sub byte [ecx], %i\n", am);
                     i += am-1;
                     continue;
                 } else {
-                    fputs("    dec byte [ecx]\n", fp);
+                    write_asm("    dec byte [ecx]\n");
                 }
             }
             else if (c == '.') {    // output char cell at pt
                 used_out_syscall = 1;
-                fputs("    call putchar\n", fp);
+                write_asm("    call putchar\n");
             }
             else if (c == ',') {    // input char cell at pt
                 used_in_syscall = 1;
-                fputs("    call getchar\n", fp);
+                write_asm("    call getchar\n");
             }
             else if (c == '[') {    // jumps after the corresponding ']' instruction if the cell val is 0
                 int corr = -1;
@@ -154,22 +153,22 @@ int main(int argc, char **argv) {
                 }
 
                 if (corr > 0) {
-                    fputs("    xor eax, eax\n", fp);
-                    fputs("    mov al, [ecx]\n", fp);
-                    fputs("    test eax, eax\n", fp);
-                    fprintf(fp, "    jz close_%i\n", corr);
-                    fprintf(fp, "open_%i:\n", corr);
+                    write_asm("    xor eax, eax\n");
+                    write_asm("    mov al, [ecx]\n");
+                    write_asm("    test eax, eax\n");
+                    writef_asm("    jz close_%i\n", corr);
+                    writef_asm("open_%i:\n", corr);
                 }
                 else {
                     warn("warning: unclosed square brackets!\n");
                 }
             }
             else if (c == ']') {    // jumps after the corresponding '[' instruction if the cell val is not 0
-                fputs("    xor eax, eax\n", fp);
-                fputs("    mov al, [ecx]\n", fp);
-                fputs("    test eax, eax\n", fp);
-                fprintf(fp, "    jnz open_%i\n", (int)i);
-                fprintf(fp, "close_%i:\n", (int)i);
+                write_asm("    xor eax, eax\n");
+                write_asm("    mov al, [ecx]\n");
+                write_asm("    test eax, eax\n");
+                writef_asm("    jnz open_%i\n", (int)i);
+                writef_asm("close_%i:\n", (int)i);
             }
             else {
                 continue;
@@ -181,51 +180,37 @@ int main(int argc, char **argv) {
         fputs("end:\n    mov eax,1\n    mov ebx,0\n    int 0x80\n\n", fp);
         if (used_in_syscall) {
             // generate read(0, pt, 1) syscall
-            fputs("getchar:\n", fp);
-            fputs("    mov eax, 3\n", fp);
-            fputs("    mov ebx, 0\n", fp);
-            fputs("    mov edx, 1\n", fp);
-            fputs("    push ecx\n", fp);
-            fputs("    int 0x80\n", fp);
-            fputs("    pop ecx\n", fp);
-            fputs("    test eax, eax\n", fp);
-            fprintf(fp, "    jnz .L%i\n", localid);
-            fputs("    mov byte [ecx], 0\n", fp);
-            fprintf(fp, ".L%i:\n", localid);
-            fputs("    ret\n\n", fp);
+            write_asm("getchar:\n");
+            write_asm("    mov eax, 3\n");
+            write_asm("    mov ebx, 0\n");
+            write_asm("    mov edx, 1\n");
+            write_asm("    push ecx\n");
+            write_asm("    int 0x80\n");
+            write_asm("    pop ecx\n");
+            write_asm("    test eax, eax\n");
+            writef_asm("    jnz .L%i\n", localid);
+            write_asm("    mov byte [ecx], 0\n");
+            writef_asm(".L%i:\n", localid);
+            write_asm("    ret\n\n");
             localid ++;
         }
         if (used_out_syscall) {
             // generate write(1, pt, 1) syscall
-            fputs("putchar:\n", fp);
-            fputs("    mov eax, 4\n", fp);
-            fputs("    mov ebx, 1\n", fp);
-            fputs("    mov edx, 1\n", fp);
-            fputs("    push ecx\n", fp);
-            fputs("    int 0x80\n", fp);
-            fputs("    pop ecx\n", fp);
-            fputs("    ret\n\n", fp);
+            write_asm("putchar:\n");
+            write_asm("    mov eax, 4\n");
+            write_asm("    mov ebx, 1\n");
+            write_asm("    mov edx, 1\n");
+            write_asm("    push ecx\n");
+            write_asm("    int 0x80\n");
+            write_asm("    pop ecx\n");
+            write_asm("    ret\n\n");
         }
     }
     fclose(fp);
     free(buff);
 
-    size_t ona = strlen(argv[argc-1])+1;
-    char *on = malloc(ona);
-    memcpy(on, argv[argc-1], ona);
     if (!gen_sources) {
-        char txt[255] = "nasm -f elf32 ";
-        strcat(txt, fn);
-        strcat(txt, " -o ");
-        strcat(txt, "temp.o");
-        strcat(txt, "; gold temp.o -b elf -n -nostdlib -O 32 --gc-sections -o ");
-        strcat(txt, on);
-        strcat(txt, "; rm temp.o; rm ");
-        strcat(txt, fn);
-        strcat(txt, "; strip ");
-        strcat(txt, on);
-        system(txt);
+        // todo: convert file with name fn to elf
     }
-    free(on);
     return 0;
 }
